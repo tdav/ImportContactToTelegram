9199036c5745bb200070a6779d414289a559983a
Tasibaev Davron <tdav@mail.ru>
2022-04-12 11:06:20 +0500

    fix

--- TelegramClient/FrmMain.cs
+++ TelegramClient/FrmMain.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TeleSharp.TL;
 using TeleSharp.TL.Contacts;
-using TeleSharp.TL.Messages;
 using TLSharp.Core;
 
 namespace TelClient
